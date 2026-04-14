8ac26e0eb393f8c8186b9b43bfa5cc01663b5103
David Lange <David.Lange@cern.ch>
2026-04-14 17:30:04 +0200

    Create entrypoint.sh for conda setup and repo clone
    Add entrypoint script for setting up conda environment and cloning repository.

---
+++ binder/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# cache the value because the shell hook step will remove it
+_CONDA_DEFAULT_ENV="hsf-india"
+
+__conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+eval "$__conda_setup"
+unset __conda_setup
+
+# Restore our "indended" default env
+conda activate "${_CONDA_DEFAULT_ENV}"
+
+git clone --recurse-submodules https://github.com/research-software-collaborations/courses-hsf-india-April2026
+
+exec "$@"
